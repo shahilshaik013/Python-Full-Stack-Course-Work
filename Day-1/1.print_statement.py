1ee44c0b244a656a847eb7078563e153cc3b2492
shahilshaik013 <shahilshaik01@gmail.com>
2026-01-30 13:02:24 +0530

    Add files via upload
    First program

---
+++ Day-1/1.print_statement.py
@@ -0,0 +1,6 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> print("NAME: Sahil Shaik")
+NAME: Sahil Shaik
+>>> print("Course Id: 013")
+Course Id: 013
